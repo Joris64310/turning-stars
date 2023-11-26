efb83147aa8c0f82a93ace007e8897c5c0e9e2d8
Joris64310 <joris64310@yahoo.fr>
2023-11-26 16:47:52 +0100

    Get Interfaces information (WIP)

--- Assets/Scripts/ExpeInterfaceManager.cs
+++ Assets/Scripts/ExpeInterfaceManager.cs
@@ -3,73 +3,114 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
-
 public class ExpeInterfaceManager : MonoBehaviour
 {
     // Interfaces
-    GameObject expParametersPanel;
-    GameObject roundParametersPanel;
+    public GameObject expParametersPanel;
+    public GameObject roundParametersPanel;
 
     // Input fields
-    TMP_InputField expeFolderInputField;
-    InputField roundDurationInSecondInputField;
-    InputField xAxisdegreesPerSecondInputField;
-    InputField yAxisdegreesPerSecondInputField;
-    InputField zAxisdegreesPerSecondInputField;
+    public TMP_InputField expeFolderInputField;
+    public TMP_InputField participantNumberInputField;
+    public TMP_InputField roundDurationInSecondInputField;
+    public TMP_InputField xAxisdegreesPerSecondInputField;
+    public TMP_InputField yAxisdegreesPerSecondInputField;
+    public TMP_InputField zAxisdegreesPerSecondInputField;
 
     // Buttons
     Button setExpeParametersButton;
     Button setRoundParametersButton;
     Button setExitButton;
 
+    // Initialize round variables
+    bool inRoundBool = false;
+    int roundNumber = 0;
+
+    System.DateTime roundStartTime;
+    System.TimeSpan roundElapsedTime;
+    System.TimeSpan roundDurationTime;
+ 
+    int xAxisdegreesPerSecondValue;
+    int yAxisdegreesPerSecondValue;
+    int zAxisdegreesPerSecondValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Interfaces
-        expParametersPanel = GameObject.Find("Expe Parameters Panel");
-        roundParametersPanel = GameObject.Find("Round Parameters Panel");
-
-        // Input fields
-        GameObject a =  GameObject.Find("Expe Folder InputField");
-        Debug.Log("here");
-        Debug.Log(a);
-
-        expeFolderInputField = GameObject.Find("expe Folder InputField").GetComponent<TMP_InputField>();
-        roundDurationInSecondInputField = GameObject.Find("Round Duration In Second InputField").GetComponent<InputField>();
-        xAxisdegreesPerSecondInputField = GameObject.Find("xAxisdegreesPerSecondInputField").GetComponent<InputField>();
-        yAxisdegreesPerSecondInputField = GameObject.Find("yAxisdegreesPerSecondInputField").GetComponent<InputField>();
-        zAxisdegreesPerSecondInputField = GameObject.Find("zAxisdegreesPerSecondInputField").GetComponent<InputField>();
-        
+        // Set active the round panel
+        roundParametersPanel.SetActive(true);
         // Buttons
-        setExpeParametersButton = GameObject.Find("setExpeParametersButton").GetComponent<Button>();
-        setRoundParametersButton = GameObject.Find("setRoundParametersButton").GetComponent<Button>();
-        setExitButton = GameObject.Find("setExitButton").GetComponent<Button>();
+        setExpeParametersButton = GameObject.Find("Set Expe Parameters Button").GetComponent<Button>();
+        setRoundParametersButton = GameObject.Find("Set Round Parameters Button").GetComponent<Button>();
+        setExitButton = GameObject.Find("Exit Button").GetComponent<Button>();
 
         // Add Listener to buttons
-        setExpeParametersButton.onClick.AddListener(TaskOnSetExpeParametersButton);
-        setRoundParametersButton.onClick.AddListener(TaskOnSetRoundParametersButton);
-        setExitButton.onClick.AddListener(TaskOnSetExitButton);
+        setExpeParametersButton.onClick.AddListener(TaskOnClickSetExpeParametersButton);
+        setRoundParametersButton.onClick.AddListener(TaskOnClickSetRoundParametersButton);
+        setExitButton.onClick.AddListener(TaskOnClickSetExitButton);
+
+        // Restrict input fields
+        participantNumberInputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+        roundDurationInSecondInputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+        xAxisdegreesPerSecondInputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+        yAxisdegreesPerSecondInputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+        zAxisdegreesPerSecondInputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+
+        //TODO: proposed a participant number
+
+
+        // Set inactive the round panel
+        roundParametersPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (inRoundBool)
+        {
+            roundElapsedTime = System.DateTime.UtcNow - roundStartTime;
+
+            if (roundElapsedTime < roundDurationTime)
+            {
+                //TODO: save data
+            }
+            else
+            {
+                roundParametersPanel.SetActive(true);
+                roundNumber += 1;
+                //TODO: Update text
+                inRoundBool = false;
+                //TODO: Save and close last data
+            }
+        }
+
         
     }
-    private void TaskOnSetExpeParametersButton()
+    private void TaskOnClickSetExpeParametersButton()
     {
         expParametersPanel.SetActive(false);
         roundParametersPanel.SetActive(true);
 
+        //TODO: Create the participant folder
+
     }
-    private void TaskOnSetRoundParametersButton()
+    private void TaskOnClickSetRoundParametersButton()
     {
+        // Get round duration
+        roundStartTime = System.DateTime.UtcNow;
+        roundDurationTime = System.TimeSpan.FromSeconds(int.Parse(roundDurationInSecondInputField.text));
+
+        // Get round rotation
+        xAxisdegreesPerSecondValue = int.Parse(xAxisdegreesPerSecondInputField.text);
+        yAxisdegreesPerSecondValue = int.Parse(yAxisdegreesPerSecondInputField.text);
+        zAxisdegreesPerSecondValue = int.Parse(zAxisdegreesPerSecondInputField.text);
+
+        //TODO: Update rotation value of the camera
+
         roundParametersPanel.SetActive(false);
 
     }
-    private void TaskOnSetExitButton()
+    private void TaskOnClickSetExitButton()
     {
          Application.Quit();
     }
